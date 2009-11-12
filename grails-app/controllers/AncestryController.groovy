dfa705a23ea304ea93b3d183ef8b24c643a67f1b
jyoung.oclc <jyoung.oclc@428cac9a-c7f1-11de-8672-e51b9359e43c>
2009-11-12 16:28:35 +0000

    Rename the model controller to match the {modelName}

--- grails-app/controllers/AncestryController.groovy
+++ grails-app/controllers/AncestryController.groovy
@@ -1,14 +1,4 @@
-/**
- * Copyright 2009 OCLC Online Computer Library Center Licensed under the Apache
- * License, Version 2.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or
- * agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
-class ModelController {
+class AncestryController {
 	// OK for now
 	def index = {
 		redirect(uri: "/")
@@ -36,6 +26,11 @@ class ModelController {
 			response.outputStream.flush()
 		}
     }
+
+	def realworldclass = {
+		// Redirect to the Real World Model URI
+		redirect(action: 'realworldmodel')
+	}
 
 	// Produce an RDF representation of the model
 	def about = {
